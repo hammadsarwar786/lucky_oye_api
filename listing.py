a989256dcc621b9cab771e90cc980048344bd5fc
hammadsarwar786 <hammadarain326@gmail.com>
2023-09-23 21:10:53 +0500

    id

--- listing.py
+++ listing.py
@@ -9,9 +9,9 @@ router = APIRouter()
 # Endpoint to upload a document
 
 @router.post("/uploadfile")
-async def upload_file(id: int = Form(...), files: List[UploadFile]= Form(...)):
+async def upload_file(id: int = Form(...), listingID: int = Form(...) ,files: List[UploadFile]= Form(...)):
     # Get the current working directory
-    id_directory = os.path.join("files", str(id))
+    id_directory = os.path.join("files", listingID,str(id))
     os.makedirs(id_directory, exist_ok=True)
     # Create a unique filename for the uploaded file by joining the current directory and the file name
 
@@ -22,16 +22,16 @@ async def upload_file(id: int = Form(...), files: List[UploadFile]= Form(...)):
         file_path = os.path.join(id_directory, file.filename)
         with open(file_path, "wb") as f:
             shutil.copyfileobj(file.file, f)
-        file_links.append(f"http://idxdubai.com:8000/files/{id}/{file.filename}")
+        file_links.append(f"http://idxdubai.com:8000/files/{listingID}/{id}/{file.filename}")
         
     # Return a response with the link to download the file
     return JSONResponse(content={"message": "Files uploaded successfully", "file_links": file_links})
 
 UPLOAD_DIR = "images"
 @router.post("/uploadimage")
-async def upload_files(id: int = Form(...), files: List[UploadFile] = Form(...)):
+async def upload_files(id: int = Form(...), listingID: int = Form(...) , files: List[UploadFile] = Form(...)):
     # Create a directory for the ID if it doesn't exist
-    id_directory = os.path.join(UPLOAD_DIR, str(id))
+    id_directory = os.path.join(UPLOAD_DIR, listingID, str(id))
     os.makedirs(id_directory, exist_ok=True)
 
     file_links = []
@@ -45,13 +45,13 @@ async def upload_files(id: int = Form(...), files: List[UploadFile] = Form(...))
             shutil.copyfileobj(file.file, f)
 
         # Add the file's link to the list of file links
-        file_links.append(f"http://idxdubai.com:8000/images/{id}/{file.filename}")
+        file_links.append(f"http://idxdubai.com:8000/images/{listingID}/{id}/{file.filename}")
 
     return JSONResponse(content={"message": "Files uploaded successfully", "file_links": file_links})
 
-@router.get("{id}/{file_name}")
-async def download_file(id: int, file_name: str):
-    id_directory = os.path.join("images", str(id))
+@router.get("{listingID}/{id}/{file_name}")
+async def download_file(id: int, listingID: int,file_name: str):
+    id_directory = os.path.join("images", str(listingID), str(id))
     file_path = os.path.join(id_directory, file_name)
       # Log the file_path to help with debugging
     logging.info(f"File path: {file_path}")
@@ -63,8 +63,8 @@ async def download_file(id: int, file_name: str):
     return FileResponse(path=file_path, filename=file_path, media_type='application/octet-stream')
 
 
-@router.get("{id}/{file_name}")
-async def download_file(id: int, file_name: str):
+@router.get("{listingID}/{id}/{file_name}")
+async def download_file(id: int, listingID: int,file_name: str):
     id_directory = os.path.join("files", str(id))
     file_path = os.path.join(id_directory, file_name)
 
